987a7ea09de49c59ea2dede2be821a377139b776
Tom Malisiewicz <tom@vision.ai>
2015-06-16 22:11:37 +0000

    Update download script

--- download_all.sh
+++ download_all.sh
@@ -7,18 +7,20 @@
 # Copyright vision.ai 2015
 
 REMOTE_URL="https://models.vision.ai"
+LOCAL_URL="http://localhost"
 
 echo "This script will download all new models from models.vision.ai"
-LOCAL_UUIDS=`curl -s https://localhost/model | jq -r '.data[] .uuid'`
+LOCAL_UUIDS=`curl -s ${LOCAL_URL}/model | jq -r '.data[] .uuid'`
 REMOTE_UUIDS=`curl -s ${REMOTE_URL}/model | jq -r '.data[] .uuid'`
 for UUID in $REMOTE_UUIDS; do
-    if [ -n `echo $LOCAL_UUIDS | grep $UUID` ]; then
+    if [ ! -n `echo $LOCAL_UUIDS | grep $UUID` ]; then
 	EXTRA="does not exist, so downloading"
 	echo $UUID $EXTRA
 	mkdir /incoming/$UUID/
-	curl -o /incoming/$UUID/data_set.json $REMOTE_URL/models/$UUID/data_set.json
 	curl -o /incoming/$UUID/image.jpg $REMOTE_URL/models/$UUID/image.jpg
+	curl -o /incoming/$UUID/model.json $REMOTE_URL/models/$UUID/model.json
 	curl -o /incoming/$UUID/model.data $REMOTE_URL/models/$UUID/model.data
+	curl -o /incoming/$UUID/data_set.json $REMOTE_URL/models/$UUID/data_set.json
 	curl -o /incoming/$UUID/compiled.data $REMOTE_URL/models/$UUID/compiled.data
 	cd /incoming/
 	tar cf $UUID.tar $UUID
