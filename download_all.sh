947e86ec5370f287ee046d5d0668ca17a9e2eca9
root <root@vultr.guest>
2015-06-16 22:07:31 +0000

    Add comments

--- download_all.sh
+++ download_all.sh
@@ -1,25 +1,34 @@
-#!/bin/sh
-#Script to download all models from models.vision.ai which we might not already have
+#!/bin/sh 
+#
+# Script to download all models from models.vision.ai which we might
+# not already have present in the local model store. The model data is
+# transferred into the local /incoming folder.
+#
+# Copyright vision.ai 2015
 
+REMOTE_URL="https://models.vision.ai"
+
+echo "This script will download all new models from models.vision.ai"
 LOCAL_UUIDS=`curl -s https://localhost/model | jq -r '.data[] .uuid'`
-REMOTE_UUIDS=`curl -s https://models.vision.ai/model | jq -r '.data[] .uuid'`
+REMOTE_UUIDS=`curl -s ${REMOTE_URL}/model | jq -r '.data[] .uuid'`
 for UUID in $REMOTE_UUIDS; do
     if [ -n `echo $LOCAL_UUIDS | grep $UUID` ]; then
-	EXTRA="does not exist"
-
+	EXTRA="does not exist, so downloading"
+	echo $UUID $EXTRA
 	mkdir /incoming/$UUID/
-	curl -o /incoming/$UUID/data_set.json https://models.vision.ai/models/$UUID/data_set.json
-	curl -o /incoming/$UUID/image.jpg https://models.vision.ai/models/$UUID/image.jpg
-	curl -o /incoming/$UUID/model.data https://models.vision.ai/models/$UUID/model.data
-	curl -o /incoming/$UUID/compiled.data https://models.vision.ai/models/$UUID/compiled.data
+	curl -o /incoming/$UUID/data_set.json $REMOTE_URL/models/$UUID/data_set.json
+	curl -o /incoming/$UUID/image.jpg $REMOTE_URL/models/$UUID/image.jpg
+	curl -o /incoming/$UUID/model.data $REMOTE_URL/models/$UUID/model.data
+	curl -o /incoming/$UUID/compiled.data $REMOTE_URL/models/$UUID/compiled.data
 	cd /incoming/
 	tar cf $UUID.tar $UUID
 	gzip $UUID.tar
 	rm -rf $UUID
 	cd - > /dev/null 2>&1
     else
-	EXTRA="exists"
+	EXTRA="already exists"
+	echo $UUID $EXTRA
     fi
-    #echo $UUID $EXTRA
+    
 done
 
