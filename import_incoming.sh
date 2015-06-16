ef2ed0b736e44f2d78bff54785f7d9a057507585
root <root@vultr.guest>
2015-06-16 19:18:14 +0000

    import all tarballs inside /incoming

---
+++ import_incoming.sh
@@ -0,0 +1,4 @@
+#Script to import all models (represented as tarballs) from the
+#/incoming folder into the vmx containers
+
+docker run --rm --name vmx-export --volumes-from vmx-userdata:rw -v /incoming:/incoming ubuntu /bin/bash -c "cp -R /incoming/* /vmx/models && cd /vmx/models/ && tar xf *.gz && rm *.gz"
